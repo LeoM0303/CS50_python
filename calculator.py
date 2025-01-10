e43cb890a7bd25e935b1f976a5999aa59242ca15
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-10 15:40:36 +0200

    first version

---
+++ calculator.py
@@ -0,0 +1,21 @@
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y    
+
+def divide(x, y):
+    return x / y
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
