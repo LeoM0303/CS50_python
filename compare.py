52d1e58cfd35ee2a6c1c048af2099a490c04e669
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-11 12:23:04 +0200

    Refactor comparison logic to improve readability and functionality

--- compare.py
+++ compare.py
@@ -1,7 +1,18 @@
 x = int(input("Enter a number: "))
 y = int(input("Enter another number: "))
 
-if x != y:
-    print("The numbers are not equal.")
-else:
-    print("The numbers are equal.")
+#def new_func(x, y):
+#    if x != y:
+#       print("The numbers are not equal.")
+#    else:
+#        print("The numbers are equal.")
+
+#new_func(x, y)
+
+def new_func(x, y):
+    if x == y:
+        print("The numbers are equal.")
+    else:
+        print("The numbers are not equal.")
+
+new_func(x, y)
