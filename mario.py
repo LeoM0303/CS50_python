afdb47e92c4178a35398dd5dec98ee5558ba82fc
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-14 14:55:16 +0200

    Add mario.py to print a square of specified size using nested loops

---
+++ mario.py
@@ -0,0 +1,13 @@
+square = int(input("Enter the size of the square: "))
+
+def main():
+    print_square(square)
+    
+def print_square(size):
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
+    
+main()
+
