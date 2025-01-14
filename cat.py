12fe4c3ffa452d766e4e8c405fb7fdedb6b8ddd2
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-14 12:19:30 +0200

    Replace while loop with for loop to print "Meow" three times for improved clarity

--- cat.py
+++ cat.py
@@ -1,5 +1,11 @@
-i = 0 
+#use while loop to print "Meow" 3 times
+# i = 0 
 
-while i < 3:
+# while i < 3:
+#     print("Meow")
+#     i += 1     
+
+
+# #use for loop to print "Meow" 3 times
+for i in range(3):
     print("Meow")
-    i += 1     
