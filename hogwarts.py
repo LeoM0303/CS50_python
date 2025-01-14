1af7b9da944dd2e65cd6eb91e09db7a34f2940ff
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-14 14:36:28 +0200

    Refactor student list to include houses and improve output format

--- hogwarts.py
+++ hogwarts.py
@@ -1,4 +1,28 @@
-students = ['Harry', 'Ron', 'Hermione', 'Neville', 'Ginny']
+# students = ['Harry', 'Ron', 'Hermione', 'Neville', 'Ginny']
 
-for i in range(len(students)):
-    print(i + 1, students[i])
+# for i in range(len(students)):
+#     print(i + 1, students[i])
+
+# students = {
+#     'Harry' : 'Gryffindor',
+#     'Draco' : 'Slytherin',
+#     'Luna' : 'Ravenclaw',
+#     'Hermonie' : 'Gryffindor',
+#     'Ron' : 'Gryffindor',
+# }
+
+# for student in students:
+#     print(student, students[student])
+
+students = [
+    {'name': 'Harry', 'house': 'Gryffindor'},
+    {'name': 'Draco', 'house': 'Slytherin'},
+    {'name': 'Luna', 'house': 'Ravenclaw'},
+    {'name': 'Hermonie', 'house': 'Gryffindor'},
+    {'name': 'Ron', 'house': 'Gryffindor'},
+]
+
+for student in students:
+    name = student['name']
+    house = student['house']
+    print(name, house, sep=' - ')
