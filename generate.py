ede23bf06edf2fac1a0292bb6fb7119e7bb66dbf
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-16 01:05:31 +0200

    Update get_rand function to use a broader data set for random selection

--- generate.py
+++ generate.py
@@ -2,11 +2,13 @@
 
 from random import choice
 
+data_base = ['heads', 'tails', 'ears', 'nose', 'eyes', 'mouth']
+
 def main():
     result  = get_rand()
     print(f'The result is {result}')
     
 def get_rand():
-    return choice(['heads', 'tails'])
+    return choice(data_base)
 
 main()
