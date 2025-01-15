0141121d8de5220ff5c41380ea502404f5b13628
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-16 01:19:50 +0200

    Update generate(shuffle).py to use a fixed dataset and improve shuffling logic

--- generate(shuffle).py
+++ generate(shuffle).py
@@ -2,11 +2,15 @@
 
 from random import shuffle 
 
-dataset = list(range(1, 101))
+dataset = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 def main():
     result = mix_list()
     print(f'The result is {result}')
     
 def mix_list():
-    
+    for result in dataset:
+        shuffle(dataset)
+        return dataset
+
+main()
