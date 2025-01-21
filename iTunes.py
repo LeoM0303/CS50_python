80e6bba17f7f65a4b6176f8880e8ed792532c3e2
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-21 14:16:14 +0200

    Format iTunes API response for better readability

--- iTunes.py
+++ iTunes.py
@@ -10,4 +10,4 @@ if len(sys.argv) != 2:
     sys.exit()
 
 responce = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term="+sys.argv[1])
-print(json.dumps(responce.json()))
+print(json.dumps(responce.json(), indent=2))
