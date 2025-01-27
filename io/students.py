7d866e12fdaa5129659a2642eb04980b2d56ace2
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-27 14:29:10 +0200

    Refactor student data processing for sorting output
    Students are now stored in a list and sorted before printing. This improves organization and ensures output is ordered, enhancing readability and usability.

--- io/students.py
+++ io/students.py
@@ -1,5 +1,9 @@
-with open('students.csv', 'r') as file:
-    for line in file:
-            name, house = line.rstrip().split(";")
-            print(f'{name} is in {house}')
+students = []
 
+with open("students.csv", "r") as file:
+    for line in file:
+        name, house = line.rstrip().split(";")
+        students.append((f'{name} is in {house}'))
+
+for student in sorted(students):
+    print(student)
