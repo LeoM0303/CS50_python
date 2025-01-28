c48683151fbe5df830098aeb711f73d9cfb19a98
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-28 19:42:50 +0200

    Refactor student data handling for improved clarity.
    Replaced tuple-based data structure with dictionaries to enhance readability and maintainability. Simplified sorting by introducing a dedicated key function. Adjusted output format for consistency and better presentation.

--- io/students.py
+++ io/students.py
@@ -1,9 +1,13 @@
 students = []
 
-with open("students.csv", "r") as file:
+with open('students.csv') as file:
     for line in file:
-        name, house = line.rstrip().split(";")
-        students.append((f'{name} is in {house}'))
+        name, house = line.rstrip().split(';')
+        student = {"name": name, "house": house}
+        students.append(student)
 
-for student in sorted(students):
-    print(student)
+def get_name(student):
+    return student["name"]
+
+for student in sorted(students, key = get_name):
+    print(f'{student["name"]} from {student["house"]}')
