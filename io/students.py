cf21838885a2e27c176045a022a7f517201c1e1f
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-29 23:50:29 +0200

    Use `csv.DictReader` for cleaner code and added headers.
    Replaced `csv.reader` with `csv.DictReader` to improve code readability and maintainability. Added headers to the students.csv file to support this change. Removed unused variable from the final print statement.

--- io/students.py
+++ io/students.py
@@ -3,9 +3,9 @@ import csv
 students = []
 
 with open('students.csv') as file:
-    reader = csv.reader(file)
-    for name, home, addresses in reader:
-        students.append({"name": name, "home": home, "addresses": addresses})
+    reader = csv.DictReader(file)
+    for row in reader:
+        students.append({"name": row["name"], "home": row['home'], "addresses": row['addresses']})
 
 for student in sorted(students, key = lambda student: student["name"]):
-    print(f'{student["name"]} from {student["home"]} has addresses: {student["addresses"]}, {reader}')
+    print(f'{student["name"]} from {student["home"]} has addresses: {student["addresses"]}')
