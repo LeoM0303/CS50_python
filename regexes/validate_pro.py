f19527b6e020dcec8c23c7466b0acbec61422558
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-31 21:42:49 +0200

    Add case-insensitive email validation for Gmail
    Updated the regex to include the `re.IGNORECASE` flag, allowing case-insensitive email verification. This ensures emails with uppercase characters are also considered valid.

--- regexes/validate_pro.py
+++ regexes/validate_pro.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What`s your email? ").strip()
 
-if re.fullmatch(r"[a-zA-Z0-9._%+-]+@gmail\.com", email):
+if re.fullmatch(r"[a-z0-9._%+-]+@gmail\.com", email, re.IGNORECASE):
     print("Valid email")
 else:
     print("Invalid email")
