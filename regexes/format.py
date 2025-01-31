70def1e8716337b083d29f3a922d2b95af1926a4
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-31 22:35:02 +0200

    Refactor name parsing with regex for improved clarity.
    Replaced manual string splitting with a regex-based approach to handle name formatting. This ensures more robust and readable code while maintaining functionality. Minor adjustments streamline the logic for better maintainability.

--- regexes/format.py
+++ regexes/format.py
@@ -1,7 +1,13 @@
 import re
-name = input("Enter your name: ").strip()
 
-if "," in name:
-    last, first = name.split(",")
-    name = f"{first.strip()} {last.strip()}"
+name = input("Enter your name: ").strip()
+name = name.title().strip()
+
+matches = re.search(r"^([\w .-]*), ([\w .-]*)$", name)
+if matches:
+    last, first = matches.groups()
+    last = matches.group(1)
+    first = matches.group(2)
+    name = f'{first} {last}'
+
 print(f'Hello, {name}')
