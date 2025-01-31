c0bfa1bc3607e7eb3b793bd1213c1d9dbbadfbcc
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-31 21:28:08 +0200

    Update email validation to specifically allow Gmail addresses
    The regex was modified to ensure only Gmail addresses are recognized as valid. This improves input validation by aligning it to Gmail-specific requirements.

--- regexes/validate_pro.py
+++ regexes/validate_pro.py
@@ -2,7 +2,7 @@ import re
 
 email = input("Enter your email: ")
 
-if re.search(".*@.*", email):
+if re.search(r".*@.*.+\.gmail", email):
     print("Valid email")
 else:
     print("Invalid email")
