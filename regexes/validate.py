de8fcd6cafd1aa6128123d7f1fa619180a3c0940
LeoM0303 <leomuliarchuk51@gmail.com>
2025-01-31 17:44:11 +0200

    Restrict email validation to Gmail addresses.
    Updated the email validation logic to only accept emails ending with "gmail.com". This ensures the system specifically targets Gmail users for validation.

--- regexes/validate.py
+++ regexes/validate.py
@@ -1,8 +1,10 @@
+#check users only with gmail email
+
 email = input("Enter your email: ").strip()
 
 username, domain = email.split("@")
 
-if username and "." in domain:
+if username and domain.endswith("gmail.com"):
     print("Valid username")
 else:
     print("Invalid username")
