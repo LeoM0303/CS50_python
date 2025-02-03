86bbd4803cab0ab480e82767f9b88449b12d39d5
LeoM0303 <leomuliarchuk51@gmail.com>
2025-02-03 21:35:57 +0200

    Refactor get_young to return a dictionary.
    Updated the get_young function to return a dictionary instead of a tuple for better readability and data handling. Adjusted the main function accordingly to use dictionary keys for accessing data.

--- OOP/young.py
+++ OOP/young.py
@@ -1,12 +1,12 @@
 def main():
     student = get_young()
-    print(f"{student[0].capitalize()} from {student[1].capitalize()} lives in a young house.")
+    print(f"{student['name'].capitalize()} from {student['home'].capitalize()} lives in a young house.")
 
 def get_young():
     student = {}
     student["name"] = input("Enter student name: ")
     student["home"] = input("Enter student hometown: ")
-    return student["name"], student["home"]
+    return student
 
 if __name__ == "__main__":
     main()
