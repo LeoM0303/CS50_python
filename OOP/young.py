21b5e5808ef9022d57af9c091ec08b330227045b
LeoM0303 <leomuliarchuk51@gmail.com>
2025-02-03 21:13:51 +0200

    Refactor program into modular functions.
    Separated logic into reusable functions `get_name` and `get_house` for better clarity and maintainability. Updated the main execution flow to use a `main()` function. This improves code structure and readability.

--- OOP/young.py
+++ OOP/young.py
@@ -1,4 +1,12 @@
-name = input("Enter your name: ")
-home = input("Enter your home: ")
+def main():
+    name = get_name()
+    house = get_house()
+    print(f"{name.capitalize()} from {house.capitalize()} lives in a young house.")
 
-print(f"Hello {name.capitalize()}, your home is {home}")
+def get_name():
+    return input("What is your name? ")
+
+def get_house():
+    return input("What is your house? ")
+
+main()
